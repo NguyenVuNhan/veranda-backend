c13ba90a04b120c77f168ef4bb24a3f5c382c40f
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-08-13 12:21:37 +0200

    Init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '856ea27b1a634074e7ce09f1b6d277bf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0708bcbc-20ed-41ba-b6ba-fd3869e3e032'
+};
