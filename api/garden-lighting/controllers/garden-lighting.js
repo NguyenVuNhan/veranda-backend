7100f592e686f82812f9fd9726f1e8327fe9d836
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-10-03 15:42:42 +0200

    Add new controller for garden lighting products

--- api/garden-lighting/controllers/garden-lighting.js
+++ api/garden-lighting/controllers/garden-lighting.js
@@ -1,8 +1,15 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async getProductsList(ctx) {
+        return await strapi.services["garden-lighting"].getProductsList({
+            _locale: "nl",
+            ...ctx.query,
+        });
+    },
+};
--- api/garden-lighting/services/garden-lighting.js
+++ api/garden-lighting/services/garden-lighting.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
+const { sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+    /**
+     * Promise to fetch list of products
+     *
+     * @return {Promise}
+     */
+    async getProductsList(params, populate) {
+        const entities = await strapi
+            .query("garden-lighting")
+            .find(params, populate);
+        const sanitizedEntities = sanitizeEntity(entities, {
+            model: strapi.models["garden-lighting"],
+        });
+
+        return sanitizedEntities.map(({ title, price, slug, productSaleInfo }) => {
+            const thumbnail = productSaleInfo.thumbnail;
+            return {
+                title,
+                price,
+                slug,
+                thumbnail: {
+                    url: thumbnail.formats.thumbnail.url,
+                    alt: thumbnail.alternativeText,
+                },
+            };
+        });
+    },
+};
